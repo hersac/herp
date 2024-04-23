5f46d161cda7b2e1858886f39bc7163f0709232f
Heri <hersac1@gmail.com>
2024-04-23 02:00:02 -0500

    Crea estructura del protecto

---
+++ src/app/layouts/pages-layout/pages-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pages-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './pages-layout.component.html',
+  styleUrl: './pages-layout.component.scss'
+})
+export class PagesLayoutComponent {
+
+}
